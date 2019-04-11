f1b54b0c42999962777d73b86fda5d1a6d0eca9c
Justin Olson <j.olson.digital@gmail.com>
2019-04-11 16:24:41 -0700

    Pre Blog Removal

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,11 +21,12 @@ class About extends Component {
         <div className="details-container">
           <p className="details-container-top">
             Best of days to you! I'm Justin and I'm a self-taught web designer
-            and developer who simply enjoys creating websites and programs from
-            nothing but a simple idea. Currently I'm located in Tucson, Arizona,
-            and have been studying coding since June 2015, as I was ready to
-            develop an actual skill set with something I've always had interest
-            in. Since then, I've done the following:
+            and developer who simply enjoys bringing imagination to reality from
+            only a simple idea. Currently I'm located in Tucson, Arizona, and
+            have been on the path to becoming a software engineer since June
+            2015. It was then that I decided that I was ready to develop an
+            actual skill set with something I've always had an interest in.
+            Since then, I've done the following:
           </p>
           <ul>
             <li>
@@ -38,18 +39,27 @@ class About extends Component {
             <li>Freelanced two websites for local businesses</li>
             <li>
               Applied for, awarded, and completed the Grow with Google Udacity
-              Nanodegree front end web developer scholarship
+              Nanodegree Front-End Web Developer Scholarship
             </li>
           </ul>
+          <p className="details-container-top">
+            In this short time, I've studied and learned a few different
+            languages. I've mainly focused on front-end web development, but
+            have also toyed with mobile apps and look forward to exploring
+            anything beyond. Languages and libraries I've studied with include:{" "}
+            <span className="langHighlight">HTML</span>,{" "}
+            <span className="langHighlight">CSS</span>,{" "}
+            <span className="langHighlight">JavaScript</span>,{" "}
+            <span className="langHighlight">jQuery</span>,{" "}
+            <span className="langHighlight">Bootstrap 3/4</span>,{" "}
+            <span className="langHighlight">Python</span>,{" "}
+            <span className="langHighlight">React.JS</span> (this site was
+            created using React.JS), and{" "}
+            <span className="langHighlight">React Native</span>.
+          </p>
           <p className="details-container-bot">
-            In this short time, I've studied and am familiar with HTML, CSS,
-            JavaScript (ES6), jQuery, Bootstrap 3/4, Python, React, Git/Github,
-            ServiceNow (I've tinkered with others, these just happen to be the
-            ones I have the most experience with currently) and continue to
-            expand my knowledge with these languages/libraries/frameworks and
-            others daily. My simple goal is to have a career designing,
-            creating, and building websites from scratch, and even push past
-            that to whatever lies beyond.
+            I'm absolutely not limited to the above, as I am always eager to
+            learn something new.
           </p>
         </div>
       </div>
