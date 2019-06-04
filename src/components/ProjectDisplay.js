99942eba66e4a6a0ae24c9d58ae1e2809f8cd6b3
8bitjustin <j.olson.digital@gmail.com>
2019-06-04 07:38:33 -0700

    Added bullet and space to github link of mobile app on ProjectDisplay

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -3,27 +3,39 @@ import React from "react";
 const ProjectDisplay = props => {
   if (props.site === "") {
     return (
-      <div className="project_card" style = {{backgroundImage: 'url(' + props.image + ')'}}>
+      <div
+        className="project_card"
+        style={{ backgroundImage: "url(" + props.image + ")" }}
+      >
         <h4 className="project_title">{props.title}</h4>
         <h5 className="project_subtitle">{props.subtitle}</h5>
-        {/* eslint-disable-next-line */}
-        <a href={props.git} className="project_git" target="_blank" style = {{width: '100%'}}>
-          Github
+
+        <a
+          href={props.git}
+          className="project_git"
+          // eslint-disable-next-line
+          target="_blank"
+          style={{ width: "100%" }}
+        >
+          &#183;&nbsp;Github&nbsp;&#183;
         </a>
       </div>
     );
   } else {
     return (
-      <div className="project_card" style = {{backgroundImage: 'url(' + props.image + ')'}}>
+      <div
+        className="project_card"
+        style={{ backgroundImage: "url(" + props.image + ")" }}
+      >
         <h4 className="project_title">{props.title}</h4>
         <h5 className="project_subtitle">{props.subtitle}</h5>
         {/* eslint-disable-next-line */}
         <a href={props.git} className="project_git" target="_blank">
-        &#183;&nbsp;Github&nbsp;&#183;
+          &#183;&nbsp;Github&nbsp;&#183;
         </a>
         {/* eslint-disable-next-line */}
         <a href={props.site} className="project_site" target="_blank">
-        &#183;&nbsp;Site&nbsp;&#183;
+          &#183;&nbsp;Site&nbsp;&#183;
         </a>
       </div>
     );
